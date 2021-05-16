c3dfff252a35b90b6f6d4aaaaefa762ec5075519
Travis Jefferson <traviscjefferson@gmail.com>
2021-05-16 13:09:19 -0400

    How the heck do fonts work

--- app/root.tsx
+++ app/root.tsx
@@ -18,7 +18,14 @@ function Document({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Source+Code+Pro:ital@0;1&display=swap"
+          rel="stylesheet"
+        />
+
         <Meta />
+
         <Links />
       </head>
       <body>
